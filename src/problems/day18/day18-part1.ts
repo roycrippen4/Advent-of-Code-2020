bcc198d15d85eeaed43efaf5449ae49a96ea6c82
roycrippen4 <roycrippen4@gmail.com>
2023-09-16 21:35:56 -0400

    solved day18-part1.

--- src/problems/day18/day18-part1.ts
+++ src/problems/day18/day18-part1.ts
@@ -3,126 +3,72 @@ import { assertUnreachable, readFileToString } from "../../utilities";
 const filepath = "src/data/day18/day18-data.txt";
 const testFilepath = "src/data/day18/day18-dataTEST.txt";
 
-const testing = true;
+const testing = false;
 
 function getData() {
-  return readFileToString(testing ? testFilepath : filepath);
+  return readFileToString(testing ? testFilepath : filepath).split("\r\n");
 }
 
-enum operationType {
-  ADD = "+",
-  MULTIPLY = "*",
-  UNKNOWN = "UNKNOWN"
-}
+const reg = /\(([^()]+)\)/gm;
 
-interface Operation {
-  arg1: number | Operation;
-  type: operationType;
-  arg2: number | Operation;
-}
+function applyOperation(total: number, split: string[]): number {
+  const operand = split[0];
+  const arg = parseInt(split[1], 10);
 
-function parseParenGroups(line: string[]): string[][] {
-  let clone = [...line];
-  const groups: string[][] = [];
-  let group: string[] = [];
-  for (let i = 0; i < clone.length; i++) {
-    const char = clone[i];
-    switch (char) {
-      case "(":
-        clone.shift();
-        i--;
-        break;
-      case ")":
-        groups.push(group);
-        const nextChar = clone[i + 1];
-        if (nextChar && isOperator(nextChar)) {
-          groups.push([nextChar]);
-          clone = clone.slice(i + 2);
-          i = -1;
-        }
-
-        group = [];
-        break;
-      default:
-        group.push(char);
-    }
-  }
-  groups.push(group);
-  return groups;
-}
-
-function isOperator(char: string): boolean {
-  return char === "+" || char === "*";
-}
-
-function isOperation(arg: number | Operation): boolean {
-  return typeof arg === "object";
-}
-
-function getOperatorType(char: string): operationType {
-  switch (char) {
+  switch (operand) {
     case "+":
-      return operationType.ADD;
+      return total + arg;
     case "*":
-      return operationType.MULTIPLY;
+      return total * arg;
     default:
-      throw new Error("Invalid operator type");
+      assertUnreachable(operand);
   }
 }
 
-function evaluateOperation(arg1: number, operator: operationType, arg2: number) {
-  switch (operator) {
-    case operationType.ADD:
-      return arg1 + arg2;
-    case operationType.MULTIPLY:
-      return arg1 * arg2;
-    default:
-      throw new Error("Invalid operator type");
-  }
-}
+function compute(string: string) {
+  let result = 0;
+  const reg = /(\d+|\*|\+|\S+)/g;
+  const split = string.split(reg).filter((x) => x !== "" && x !== " ");
 
-function evaluateGroup(group: string[], result?: number): number {
-  if (group.length === 0) {
-    if (result === undefined) {
-      throw new Error("Invalid group");
+  if (split.length === 1) {
+    return split[0];
+  }
+
+  while (split.length > 0) {
+    if (result === 0) {
+      result = parseInt(split[0], 10);
+      split.shift();
     }
-    return result;
+    const slice = split.slice(0, 2);
+    result = applyOperation(result, slice);
+    split.splice(0, 2);
   }
-  if (result && isOperator(group[0])) {
-    const operator = getOperatorType(group[0]);
-    const arg2 = parseInt(group[1]);
-    const newGroup = group.slice(2);
-    return evaluateGroup(newGroup, evaluateOperation(result, operator, arg2));
-  } else {
-    const arg1 = parseInt(group[0]);
-    const newGroup = group.slice(1);
-    return evaluateGroup(newGroup, arg1);
-  }
+  return result.toString();
 }
 
-function evaluate(groups: string[][]) {
-  const newGroup: string[] = [];
-  for (let i = 0; i < groups.length; i++) {
-    const group = groups[i];
-    if (group.length === 1) {
-      newGroup.push(group[0]);
-    } else {
-      const result = evaluateGroup(group);
-      newGroup.push(result.toString());
-    }
+function computeLine(line: string) {
+  while (line.includes("(")) {
+    line = line.replace(reg, (_, p1) => {
+      return compute(p1);
+    });
   }
-  return evaluateGroup(newGroup);
+  return compute(line);
+}
+
+function computeAnswer(data: string[]) {
+  let total = 0;
+  for (const line of data) {
+    const result = parseInt(computeLine(line), 10);
+    // console.log(result);
+    total += result;
+  }
+  return total;
 }
 
 function solveD18P1() {
   const data = getData();
-  const parsed = parseParenGroups(
-    data.split("").filter((char) => {
-      return char.length !== 0 && char !== " ";
-    })
-  );
-  console.log(evaluate(parsed));
-  // console.log(JSON.stringify(parseGroups(parsed)));
+  const answer = computeAnswer(data);
+  console.log(`Day 18 Part 1 Answer: ${answer}`);
 }
 
 solveD18P1();
